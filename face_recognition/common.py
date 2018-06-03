790c7b411d8339d55a33baba8c204ef193b79265
whgaara <695509060@qq.com>
2018-06-03 17:24:25 +0800

    'up'

--- face_recognition/common.py
+++ face_recognition/common.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 import os
+import math
 import random
 import cv2
 from FaceRecognition import settings
@@ -26,12 +27,28 @@ def format_logs():
             acu_attrs = acupoint.split(',', 1)
             coord = coord_to_scale(acu_attrs[1], log_file)
             acupoints_dict[acu_attrs[0]] = coord
-            print(acupoints_dict)
+        print(acupoints_dict)
         acupoints_list.append(acupoints_dict)
         f.close()
         index += 1
     # print(acupoints_list)
     return acupoints_list
+
+
+def format_log(log_file):
+    f = open(settings.LOG_PATH + log_file, 'r', encoding='utf_8_sig')
+    log_data = f.read()
+    acupoints = log_data.split('\n')
+    acupoints_dict = {}
+    for acupoint in acupoints:
+        if acupoint == '':
+            continue
+        acu_attrs = acupoint.split(',', 1)
+        coord = coord_to_scale(acu_attrs[1], log_file)
+        acupoints_dict[acu_attrs[0]] = coord
+    f.close()
+    print(acupoints_dict)
+    return acupoints_dict
 
 
 def coord_to_scale(data, log_file):
@@ -74,6 +91,26 @@ def origin_point(img_name):
     return x, y, unit
 
 
+def origin_point_up(img_name):
+    eyes_coords = []
+    image_path = settings.TARGET_PATH + img_name
+    face_cascade = cv2.CascadeClassifier(settings.STATIC_PATH + 'classify/haarcascade_eye.xml')
+    img = cv2.imread(image_path)
+    if type(img) != str:
+        eyes = face_cascade.detectMultiScale(img, 1.1, 9)
+        if len(eyes):
+            for (x, y, w, h) in eyes:
+                if w >= 70 and h >= 70:
+                    eyes_coords.append((x + w/2, y))
+
+    x = (eyes_coords[0][0] + eyes_coords[1][0]) / 2.0
+    y = (eyes_coords[0][1] + eyes_coords[1][1]) / 2.0
+
+    point = x, y
+
+    return point
+
+
 def operate_img(img_name):
     try:
         image_path = settings.SOURCE_PATH + img_name
@@ -97,6 +134,28 @@ def operate_img(img_name):
         print("Error")
 
 
+def get_similar(org_point):
+    min_dis = ['', 0]
+    for i, img in enumerate(os.listdir(settings.IMG_BASE_PATH)):
+        point = origin_point(img)[:2]
+        if i == 0:
+            min_dis[0] = img
+            min_dis[1] = sqrt(org_point, point)
+        else:
+            if sqrt(org_point, point) - min_dis[1] < 0:
+                min_dis[0] = img
+                min_dis[1] = sqrt(org_point, point)
+    return min_dis
+
+
+def sqrt(point1, point2):
+    return math.sqrt(pow(abs(point1[0] - point2[0]), 2) + pow(abs(point1[1] - point2[1]), 2))
+
+
+
 if __name__ == '__main__':
-    format_logs()
+    # format_logs()
+    format_log('2.jpg.log')
+    # get_similar((200, 180))
+    # sqrt((1, 2), (3.3, 5.5))
     # print(origin_point('0.jpg'))
